917be91cd35e7b988af84edfc7dc684009b04b5b
Anthony Ahumada <locopala99@gmail.com>
2021-07-29 08:13:34 +0200

    second version

---
+++ assets/Js/script.js
@@ -0,0 +1,111 @@
+let preguntas = [
+  {
+    pregunta: "1. La franquicia nació de la mano de Capcom y del desarrollador Shinji Mikami, quien bautizó a la saga en el territorio nipón como Biohazard. Por temas de derechos, en occidente fue rebautizada como Resident Evil. ¿Cómo se escogió el nombre?",
+    opcion: ["A.Fue idea de Shinji Mikami y el equipo de desarrollo", "B.La filial de Capcom en Estados Unidos sugirió el nombre", "C.Se decidió mediante un concurso en las oficias de Capcom", "D.Era una de las opciones para nombrar a la franquicia en lugar de Biohazard"],
+    respuesta: 3,
+  },
+  {
+    pregunta: "prueba",
+    opcion: ["AAA", "bbb", "cc", "dd"],
+    respuesta: 2,
+  },
+];
+
+let index = 0;
+let puntaje = 0;
+let intento = 0;
+let mal = 0;
+let repetir = true;
+let puntos = document.getElementById("cajapuntos");
+let span1 = document.getElementById("span1");
+let span2 = document.getElementById("span2");
+let span3 = document.getElementById("span3");
+let span4 = document.getElementById("span4");
+let cajatiempo = document.getElementById("cajatiempo");
+let titulopreg = document.getElementById("titulo");
+let opcion = document.querySelectorAll(".opcion");
+let boton = document.getElementById("siguiente");
+
+function imprimirpreguntas(i) {
+  span1.innerHTML = preguntas[i].opcion[0];
+  span2.innerHTML = preguntas[i].opcion[1];
+  span3.innerHTML = preguntas[i].opcion[2];
+  span4.innerHTML = preguntas[i].opcion[3];
+  titulopreg.innerHTML = preguntas[i].pregunta;
+}
+
+imprimirpreguntas(index);
+
+opcion.forEach(function (opcion) {
+  opcion.addEventListener("click", () => {
+    validar(opcion);
+  });
+});
+
+boton.onclick = function(){
+  alert("diste clic");
+  siguiente(opcion);
+}
+
+function siguiente(opcion){
+  if(index>=(preguntas.length-1 )){
+    alert("acabaste pendeja");
+  }
+  index ++;
+  repetir = true;
+  imprimirpreguntas(index);
+  opcion.forEach(function(opcion){
+    opcion.classList.remove('correcto');
+    opcion.classList.remove('incorrecto');
+  });
+  
+}
+
+
+function validar(opcion) {
+    intento ++;
+
+  op1 = opcion.dataset.opt;
+
+ 
+
+ while (repetir) {
+    if (op1 == preguntas[index].respuesta) {
+        alert("esa es la respuesta correcta");
+        puntaje++;
+        opcion.classList.add('correcto');
+        puntos.innerHTML = puntaje;
+        repetir = false;
+        console.log(op1);
+        break;
+      }
+      else{
+          mal ++;
+          opcion.classList.add('incorrecto');
+          repetir = false;
+        break;        
+      }
+    
+      
+ }
+
+}
+
+function temporizador(){
+  let tiempo = 200;
+  let min = 0;
+  let seg = 0;
+  let contador = 0;
+  let timer = setInterval(function(){
+    contador ++;
+    min = Math.floor((tiempo-contador)/60);
+    seg = tiempo - min * 60 - contador;
+    cajatiempo.innerHTML = `${min} : ${seg}`
+  },1000);
+
+  if(contador==tiempo)clearInterval(timer);
+  
+
+}
+
+temporizador();
